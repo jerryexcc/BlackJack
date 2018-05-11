71a792e41f6cf9dcf1150c756d181c5cf3ecddd0
JerryChou <jerrychou@infinitus-tech.com>
2018-05-11 16:57:31 +0800

    Animation complete

--- BlackJack/ViewController.swift
+++ BlackJack/ViewController.swift
@@ -7,59 +7,165 @@
 //
 
 import UIKit
+import GameKit
 
 class ViewController: UIViewController {
     @IBOutlet var playerPos: [UIImageView]!
     @IBOutlet var npcPos: [UIImageView]!
     @IBOutlet weak var cardStartPos: UIImageView!
     var playerHand = 0
-    var cardView: UIView!
+    var playerHandCardArray = [String]()
+    var pnpcHandCardArray = [String]()
+    let card = ["card-a01", "card-a02", "card-a03", "card-a04", "card-a05", "card-a06", "card-a07", "card-a08", "card-a09", "card-a10", "card-a11", "card-a12", "card-a13"]
     
+    //測試按鈕
+    @IBOutlet var playerCardSet: [UIButton]!
+    @IBOutlet var npcCardSet: [UIButton]!
+    
+    var btn: UIButton?
+    var front: UIImageView!
+    var back: UIImageView!
+    
+    var isOpen = false
+    
+    @IBAction func testBTNAct(_ sender: UIButton) {
+        if sender.center.x == playerCardSet[0].center.x{
+            if isOpen {
+                isOpen = false
+                playerCardSet[0].setBackgroundImage(#imageLiteral(resourceName: "card-e03"), for: .normal)
+                UIView.transition(with: playerCardSet[0], duration: 1, options: .transitionFlipFromLeft, animations: nil, completion: nil)
+            }else{
+                isOpen = true
+                let pic: UIImage!
+                pic = UIImage(named: card[GKRandomDistribution(lowestValue: 0, highestValue: 12).nextInt()])
+                playerCardSet[0].setBackgroundImage(pic, for: .normal)
+                UIView.transition(with: playerCardSet[0], duration: 1, options: .transitionFlipFromLeft, animations: nil, completion: nil)
+            }
+        } else {
+            print("不能轉得牌")
+        }
+    }
+    
+    @IBAction func btnTestDismiss(_ sender: Any) {
+        if playerCardSet[0].center.x != cardStartPos.frame.minX{
+            for pos in 0...4{
+                print("\(pos)")
+                playerCardSet[pos].center.x = cardStartPos.center.x
+                playerCardSet[pos].center.y = cardStartPos.center.y
+                npcCardSet[pos].center.x = cardStartPos.center.x
+                npcCardSet[pos].center.y = cardStartPos.center.y
+                
+                playerCardSet[pos].setBackgroundImage(#imageLiteral(resourceName: "card-e03"), for: .normal)
+                npcCardSet[pos].setBackgroundImage(#imageLiteral(resourceName: "card-e03"), for: .normal)
+            }
+            self.playerHand = 0
+        }else{
+            print("沒有牌")
+        }
+    }
     @IBAction func btnGiveAct(_ sender: Any) {
-        cardView = UIView(frame: CGRect(x: cardStartPos.frame.minX, y: cardStartPos.frame.minY, width: cardStartPos.bounds.maxX, height: cardStartPos.bounds.maxY))
-        let testView: UIImageView = UIImageView(frame: CGRect(x: cardStartPos.frame.minX, y: cardStartPos.frame.minY, width: cardStartPos.bounds.maxX, height: cardStartPos.bounds.maxY))
-        testView.image = #imageLiteral(resourceName: "card-e03")
-        let test2View: UIImageView = UIImageView(frame: CGRect(x: cardStartPos.frame.minX, y: cardStartPos.frame.minY, width: cardStartPos.bounds.maxX, height: cardStartPos.bounds.maxY))
-        test2View.image = #imageLiteral(resourceName: "card-a01")
-        view.addSubview(testView)
-        UIView.animate(withDuration: 1.0) {
-            switch self.playerHand{
+        switch self.playerHand{
+        case 0:
+            self.playerHand += 1
+            self.giveCard(player: "player", position: 0)
+            self.giveCard(player: "ncp", position: 0)
+            self.giveCard(player: "player", position: 1)
+            self.giveCard(player: "ncp", position: 1)
+        case 1:
+            self.playerHand += 1
+            self.giveCard(player: "player", position: 2)
+            self.giveCard(player: "ncp", position: 2)
+        case 2:
+            self.playerHand += 1
+            self.giveCard(player: "player", position: 3)
+            self.giveCard(player: "ncp", position: 3)
+        case 3:
+            self.playerHand = 0
+            self.giveCard(player: "player", position: 4)
+            self.giveCard(player: "ncp", position: 4)
+        default:
+            self.playerHand = 0
+        }
+    }
+    
+    func giveCard(player: String, position: Int){
+        if player == "player"{
+            switch position {
             case 0:
-                testView.center.x = (self.playerPos.first?.center.x)!
-                testView.center.y = (self.playerPos.first?.center.y)!
-                self.playerHand += 1
+                UIView.animate(withDuration: 1.0) {
+                    self.playerCardSet[0].center.x = (self.playerPos[0].center.x)
+                    self.playerCardSet[0].center.y = (self.playerPos[0].center.y)
+                }
             case 1:
-                testView.center.x = (self.playerPos[1].center.x)
-                testView.center.y = (self.playerPos[1].center.y)
-                self.playerHand += 1
+                UIView.animate(withDuration: 1.0) {
+                    self.playerCardSet[1].center.x = (self.playerPos[1].center.x)
+                    self.playerCardSet[1].center.y = (self.playerPos[1].center.y)
+                    self.openCard(position: 1)
+                }
             case 2:
-                testView.center.x = (self.playerPos[2].center.x)
-                testView.center.y = (self.playerPos[2].center.y)
-                self.playerHand += 1
+                UIView.animate(withDuration: 1.0) {
+                    self.playerCardSet[2].center.x = (self.playerPos[2].center.x)
+                    self.playerCardSet[2].center.y = (self.playerPos[2].center.y)
+                    self.openCard(position: 2)
+                }
             case 3:
-                testView.center.x = (self.playerPos[3].center.x)
-                testView.center.y = (self.playerPos[3].center.y)
-                self.playerHand += 1
+                UIView.animate(withDuration: 1.0) {
+                    self.playerCardSet[3].center.x = (self.playerPos[3].center.x)
+                    self.playerCardSet[3].center.y = (self.playerPos[3].center.y)
+                    self.openCard(position: 3)
+                }
             case 4:
-                testView.center.x = (self.playerPos[4].center.x)
-                testView.center.y = (self.playerPos[4].center.y)
-                self.playerHand = 0
+                UIView.animate(withDuration: 1.0) {
+                    self.playerCardSet[4].center.x = (self.playerPos[4].center.x)
+                    self.playerCardSet[4].center.y = (self.playerPos[4].center.y)
+                    self.openCard(position: 4)
+                }
             default:
-                self.playerHand = 0
+                print("Nothing")
+            }
+        }else{
+            //NPC's Position
+            switch position {
+            case 0:
+                UIView.animate(withDuration: 1.0) {
+                    self.npcCardSet[0].center.x = self.npcPos[0].center.x
+                    self.npcCardSet[0].center.y = self.npcPos[0].center.y
+                }
+            case 1:
+                UIView.animate(withDuration: 1.0) {
+                    self.npcCardSet[1].center.x = self.npcPos[1].center.x
+                    self.npcCardSet[1].center.y = self.npcPos[1].center.y
+                }
+            case 2:
+                UIView.animate(withDuration: 1.0) {
+                    self.npcCardSet[2].center.x = self.npcPos[2].center.x
+                    self.npcCardSet[2].center.y = self.npcPos[2].center.y
+                }
+            case 3:
+                UIView.animate(withDuration: 1.0) {
+                    self.npcCardSet[3].center.x = self.npcPos[3].center.x
+                    self.npcCardSet[3].center.y = self.npcPos[3].center.y
+                }
+            case 4:
+                UIView.animate(withDuration: 1.0) {
+                    self.npcCardSet[4].center.x = self.npcPos[4].center.x
+                    self.npcCardSet[4].center.y = self.npcPos[4].center.y
+                }
+            default:
+                print("Nothing")
             }
         }
     }
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    func openCard(position: Int){
+        playerCardSet[position].setBackgroundImage(#imageLiteral(resourceName: "card-a02"), for: .normal)
+        UIView.transition(with: playerCardSet[position], duration: 1, options: .transitionFlipFromLeft, animations: nil, completion: nil)
+        npcCardSet[position].setBackgroundImage(#imageLiteral(resourceName: "card-a04"), for: .normal)
+        UIView.transition(with: npcCardSet[position], duration: 1, options: .transitionFlipFromLeft, animations: nil, completion: nil)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func showDown(){
+        npcCardSet[0].setBackgroundImage(#imageLiteral(resourceName: "card-a04"), for: .normal)
+        UIView.transition(with: npcCardSet[0], duration: 1, options: .transitionFlipFromLeft, animations: nil, completion: nil)
     }
-
-
 }
 
